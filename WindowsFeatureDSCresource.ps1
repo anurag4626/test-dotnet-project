42c818a2eb7f7c68e6fe3878ff6e1d06474cbf63
anurag4626 <anurag.sharma4626@gmail.com>
2022-12-23 13:45:22 +0530

    v1.0

---
+++ WindowsFeatureDSCresource.ps1
@@ -0,0 +1,15 @@
+﻿configuration Name
+{
+    # One can evaluate expressions to get the node list
+    # E.g: $AllNodes.Where("Role -eq Web").NodeName
+    node ("Node1","Node2","Node3")
+    {
+        # Call Resource Provider
+        # E.g: WindowsFeature, File
+        WindowsFeature FriendlyName
+        {
+           Ensure = "Present"
+           Name   = "Feature Name"
+        }      
+    }
+}
